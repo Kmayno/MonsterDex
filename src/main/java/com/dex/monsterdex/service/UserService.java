eb5194f55ad2d3415deb7e3843c9162a677f63d0
Kmayno <carminenigro4@gmail.com>
2026-05-27 16:21:53 +0200

    Add Userservice

--- src/main/java/com/dex/monsterdex/service/UserService.java
+++ src/main/java/com/dex/monsterdex/service/UserService.java
@@ -1,7 +1,32 @@
 package com.dex.monsterdex.service;
 
+import com.dex.monsterdex.pojo.User;
+import com.dex.monsterdex.repository.MonsterRepository;
+import com.dex.monsterdex.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(int id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User getUser(String email) {
+        return userRepository.findByEmail(email).orElse(null);
+    }
+
 }
